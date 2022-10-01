f549322548227cad0dc550020c7eb4794da42270
bbchip13 <bbchip13@pulse9.net>
2022-10-02 01:50:05 +0900

    Update files

--- run_demo.py
+++ run_demo.py
@@ -85,9 +85,11 @@ class Demo(nn.Module):
         self.gen.eval()
 
         print('==> loading data')
-        self.save_path = args.save_folder + '/%s' % args.model
-        os.makedirs(self.save_path, exist_ok=True)
-        self.save_path = os.path.join(self.save_path, Path(args.source_path).stem + '_' + Path(args.driving_path).stem + '.mp4')
+        # self.save_path = args.save_folder + '/%s' % args.model
+        self.save_path = args.save_folder
+        if not self.save_path.endswith('.mp4'):
+            os.makedirs(self.save_path, exist_ok=True)
+            self.save_path = os.path.join(self.save_path, Path(args.source_path).stem + '_' + Path(args.driving_path).stem + '_LIA' + '.mp4')
         self.img_source = img_preprocessing(args.source_path, args.size).cuda()
         self.vid_target, self.fps = vid_preprocessing(args.driving_path, args.size)
         self.vid_target = self.vid_target.cuda()
